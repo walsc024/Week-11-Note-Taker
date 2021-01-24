e10096c4fbb6814abc358f29a948750de5cffc79
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-24 19:18:04 +0000

    fixed issue with store module

--- db/store.js
+++ db/store.js
@@ -1,12 +1,33 @@
 const fs = require("fs")
-const util = require("util");
+const util = require("util"); 
+const { v4: uuidv4 } = require('uuid');
 const promisifyReadFile = util.promisify(fs.readFile);
+const promisifyWriteFile = util.promisify(fs.writeFile);
 class databaseFunctionality {
-    getNotes(){
-        const dbContents = promisifyReadFile("db/db.json");
-        console.log(dbCOntents)
+    read() {
+        return promisifyReadFile("db/db.json", "utf8");
     }
-    addNote(){}
-    deleteNote(){}
+    write(myNotes) {
+        return promisifyWriteFile("db/db.json", JSON.stringify(myNotes));
+    }
+    getNotes(){
+        return this.read().then((myNotes) => {
+            let jsonNotes = [].concat(JSON.parse(myNotes))
+            return jsonNotes;
+        })
+    }
+    addNote(myNewNote){
+       const { title, text } = myNewNote;
+       const newNote = { title, text, id: uuidv4()  };
+       return this.getNotes()
+       .then((myNotes) => [...myNotes, newNote])
+       .then((updatedNotes) => this.write(updatedNotes))
+       .then(() => newNote);
+    }
+    deleteNote(idOnNoteToDelete){
+        return this.getNotes()
+        .then((myNotes) => myNotes.filter((currentNote) => currentNote.id !== idOnNoteToDelete))
+        .then((updatedNotes) => this.write(updatedNotes))
+    }
 }
 module.exports = new databaseFunctionality();
