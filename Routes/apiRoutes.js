e9ac4ee83931803a2921b2a0a54e5b27a5b3ecd9
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-24 19:16:46 +0000

    fixed issue for delete function

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,61 +1,27 @@
-const router = require("express").Router();
-const path = require("path");
+//create a router
+const express = require("express");
+const databaseFunctions = require("../db/database");
+const router = express.Router();
 const fs = require("fs");
-//const DB = fs.readFile("../db/db.json")
-
-module.exports = function(app) {
-
-    app.get("/api/notes", function(req, res){
-
-        res.json(notes);
-    });
-    
-
-// API Routes 
-// =================================
-
-router.get("/api/notes", (request, response) => {
-    // use fs to read db.json file and return notes as json file
-    response.json(notes)
-})
-
-router.get("/api/notes/:id" ,function(req, res) {
-    res.json(notes[req.params.id]);
+//add a get request to it
+router.get("/notes", (req, res) => {
+  databaseFunctions.getNotes().then((myNotes) => {
+    res.json(myNotes);
+  });
+  // Should read the `db.json` file and return all saved notes as JSON
 });
-
-router.post("/api/notes", (request, response) =>{
-    // recieve a new note to use on the request body
-
-    let newNote = req.body;
-    let uniqueId = (data.length).toString();
-    console.log(uniqueId);
-    mewNote.id = uniqueId;
-    notes.push(newNote);
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err){
-        if (err) throw (err);
-    });
-
-    res.json(notes); 
+//add a post request to it
+router.post("/notes", (req, res) => {
+  databaseFunctions.addNote(req.body).then(() => {
+    res.sendStatus(200)
+  })
 });
-
-router.delete("/api/notes:id", (request, response) => {
-    // recieve the query parameter id for a request to delete
-    
-    let noteId = req.params.id;
-        let newId = 0;
-        console.log(`Deleting note with id ${noteId}`);
-        notes = notes.filter(currentNote => {
-            return currentNote.id != noteId;
-        });
-        for (currentNote of notes) {
-            currentNote.id = newId.toString();
-            newId++;
-        }
-        fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-        res.json(notes);
+//add a delete request to it
+router.delete("/notes/:id", (req, res) => {
+  let noteId = req.params.id;
+  databaseFunctions.deleteNote(req.params.id).then(() => {
+    res.sendStatus(200)
+  })
 });
-
-}
-
-module.exports = router
+//export the router
+module.exports = router;
