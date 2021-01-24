80b7ce59ffaf1fbb1f2f13ed0854ebd5cf27af5f
walsc024 <66418697+walsc024@users.noreply.github.com>
2021-01-24 19:18:23 +0000

    swapped lines 15 and 16 to make work

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-app.use("/", html)
 app.use("/", api)
+app.use("/", html)
+
 app.listen(PORT, () => {
     console.log("Server Running")
 })
